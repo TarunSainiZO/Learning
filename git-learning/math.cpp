6ea54bcee6c0265baa2491c90b510d57a750f4d2
TarunSainiZO <tarun.saini@winzogames.com>
2021-06-01 15:44:44 +0530

    add sum

---
+++ git-learning/math.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"enter two numbers : ";
+    cin>>a>>b;
+    cout<<"sum = "<<a+b<<"\n";
+    return 0;
+}
