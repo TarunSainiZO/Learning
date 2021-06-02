a1f031430272485e2bed3c4fe3bc3010dd106c78
TarunSainiZO <tarun.saini@winzogames.com>
2021-06-02 11:40:42 +0530

    final working

--- recyclerview/app/src/main/java/com/win95/recyclerview/MainActivity.kt
+++ recyclerview/app/src/main/java/com/win95/recyclerview/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val recyclerView: RecyclerView = findViewById(R.id.rvUsers)
         recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.itemAnimator = DefaultItemAnimator()
         getData(recyclerView)
 //        val data = FakeData().data
 //        recyclerView.adapter = MyAdapter(data,this@MainActivity)
